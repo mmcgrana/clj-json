0975b99fd6fc9b29c925c1b0e77e0b8ff9b281c5
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-06 11:04:46 -0500

    Release version 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-json "0.2.0-SNAPSHOT"
+(defproject clj-json "0.2.0"
   :description "Fast JSON encoding and decoding for Clojure via the Jackson library."
   :url "http://github.com/mmcgrana/clj-json"
   :source-path "src/clj"
@@ -7,5 +7,5 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [org.codehaus.jackson/jackson-core-asl "1.4.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [lein-javac "0.0.2-SNAPSHOT"]])
+  :dev-dependencies [[org.clojars.mmcgrana/lein-clojars "0.5.0"]
+                     [org.clojars.mmcgrana/lein-javac "0.1.0"]])
