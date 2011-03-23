6ac05dc79962b1e5995bf950ee781e59a20bcb39
Mark McGranaghan <mmcgrana@gmail.com>
2011-03-22 21:52:44 -0700

    0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-json "0.3.1"
+(defproject clj-json "0.3.2"
   :description "Fast JSON encoding and decoding for Clojure via the Jackson library."
   :url "http://github.com/mmcgrana/clj-json"
   :source-path "src/clj"
