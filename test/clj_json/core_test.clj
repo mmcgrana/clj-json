b9245bfe0548ae596e488895d11417e05cc8d192
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2011-12-13 06:16:25 +0900

    use java.util.Map instead of IPersistentMap for checking type

--- test/clj_json/core_test.clj
+++ test/clj_json/core_test.clj
@@ -1,12 +1,14 @@
 (ns clj-json.core-test
   (:use clojure.test)
   (:require [clj-json.core :as json])
-  (:import (java.io StringReader BufferedReader)))
+  (:import (java.io StringReader BufferedReader)
+           (java.util HashMap)))
 
 (deftest test-string-round-trip
   (let [obj {"int" 3 "long" 52001110638799097 "bigint" 9223372036854775808
              "double" 1.23 "boolean" true "nil" nil "string" "string"
-             "vec" [1 2 3] "map" {"a" "b"} "list" (list "a" "b")}]
+             "vec" [1 2 3] "map" {"a" "b"} "list" (list "a" "b")
+             "hmap" (HashMap. {"a" "b"})}]
     (is (= obj (json/parse-string (json/generate-string obj))))))
 
 (deftest test-generate-accepts-float
