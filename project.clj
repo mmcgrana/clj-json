feda0f24c4fb7ae0c5c27a328ac047f8a5f9bcc5
Tom Denley <tom.denley@youdevise.com>
2012-08-06 15:22:31 +0100

    update project.clj to work with leiningen v2, and update clojure and jackson dependencies to latest versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-json "0.5.1"
   :description "Fast JSON encoding and decoding for Clojure via the Jackson library."
   :url "http://github.com/mmcgrana/clj-json"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
   :javac-fork "true"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.codehaus.jackson/jackson-core-asl "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.codehaus.jackson/jackson-core-asl "1.9.9"]])
