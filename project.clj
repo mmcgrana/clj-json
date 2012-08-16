47b280a963e03bff97785db8b7ec1d6c60bdaf34
lance bradley <l@ncebradley.org>
2012-08-15 18:54:21 -0700

    v 0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-json "0.5.1"
+(defproject clj-json "0.5.2"
   :description "Fast JSON encoding and decoding for Clojure via the Jackson library."
   :url "http://github.com/mmcgrana/clj-json"
   :source-paths ["src/clj"]
